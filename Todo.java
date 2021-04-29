2a99e34c4d5dcd8ba7f78a149d8e1f0ddf8438b0
TimmyLuong11 <Timothy.luong@ou.edu>
2021-04-28 22:37:08 -0500

    Initial commit

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-03
+OU ID_______________: 113401608
+OU EMAIL____________: timothy.luong@ou.edu
+
+Tasks:
+1. Complete method and getters
+2. Complete toString in Equipment
+3. Complete reader and writer 
+4. Complete method 
+5. Finish the format
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 30
+4. 30
+5. 10
+
+Actual Time (minutes):
+1. 5
+2. 2
+3. 60
+4. 60
+5. 10
+*/}
